9db324cf5a6429fd750797432d6b2d42f7f7e8fb
Belit Wilson <belitwilson@gmail.com>
2019-09-08 17:42:45 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,4 +1,3 @@
 def #display_rainbow 
-  #display_rainbow(colors)
-  puts colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
+  colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
   end
