32b5f9a8ce48f906ca20eef01cd8a733ec6dc09f
Belit Wilson <belitwilson@gmail.com>
2019-09-08 17:55:58 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,10 +1,10 @@
 def #display_rainbow 
   colors( #{"red", "orange", "yellow", "green", "blue", "indigo", "violet"})
   puts "the first color in the rainbow is : #{colors [0]}"
-  puts "the second color in the rainbow is : #{orange}"
-  puts "the third color in the rainbow is : #{yellow}"
-  puts "the fourth color in the rainbow is : #{green}"
-  puts "the fifth color in the rainbow is : #{blue}"
-  puts "the sixth color in the rainbow is : #{indigo}"
-  puts "the seventh color in the rainbow is : #{violet}"
+  puts "the second color in the rainbow is : #{colors [1]}"
+  puts "the third color in the rainbow is : #{colors [2]}"
+  puts "the fourth color in the rainbow is : #{colors [3]}"
+  puts "the fifth color in the rainbow is : #{colors [4]}"
+  puts "the sixth color in the rainbow is : #{colors [5]}"
+  puts "the seventh color in the rainbow is : #{colors [6]}"
   end
